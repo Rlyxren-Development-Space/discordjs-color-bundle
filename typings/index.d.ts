d12978fddbf05d1b6fc409c73e19466dd5cd0505
Alpha5959 <109584578+Alpha5959@users.noreply.github.com>
2023-03-08 15:24:11 +0600

    (+) Added 5 More Colors

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,35 +1,9 @@
 /**
  * - The color type for Discord.js
- * - It has 25 colors with number type
+ * - It has 30 colors with number type
  */
 
-type colorsType = {
-  Red: number;
-  Orange: number;
-  Yellow: number;
-  Green: number;
-  Blue: number;
-  Indigo: number;
-  Violet: number;
-  Purple: number;
-  Lime: number;
-  LemonJuice: number;
-  CadetBlue: number;
-  HotPink: number;
-  CornFlowerBlue: number;
-  Brick: number;
-  OliveDrab: number;
-  Olive: number;
-  Coral: number;
-  LightCoral: number;
-  Cyan: number;
-  LightCyan: number;
-  Aqua: number;
-  Aquamarine: number;
-  Salmon: number;
-  SeaGreen: number;
-  SeaBlue: number;
-  Colors: {
+ type colorsType = {
     Red: number;
     Orange: number;
     Yellow: number;
@@ -55,24 +29,55 @@ type colorsType = {
     Salmon: number;
     SeaGreen: number;
     SeaBlue: number;
+    Colors: {
+      Red: number;
+      Orange: number;
+      Yellow: number;
+      Green: number;
+      Blue: number;
+      Indigo: number;
+      Violet: number;
+      Purple: number;
+      Lime: number;
+      LemonJuice: number;
+      CadetBlue: number;
+      HotPink: number;
+      CornFlowerBlue: number;
+      Brick: number;
+      OliveDrab: number;
+      Olive: number;
+      Coral: number;
+      LightCoral: number;
+      Cyan: number;
+      LightCyan: number;
+      Aqua: number;
+      Aquamarine: number;
+      Salmon: number;
+      SeaGreen: number;
+      SeaBlue: number;
+      LightSeaGreen: number;
+      SkyBlue: number;
+      LightSkyBlue: number;
+      SpringGreen: number;
+      Tomato: number;
+    };
   };
-};
-/**
- * - The color object for Discord.js
- * - It has 25 colors
- * - It only works with Discord.js
- */
-declare const colors: colorsType;
-
-/**
- * - 25 Colors
- * @param {number} amount
- * @returns all the colors name
- * @example ```js
- * console.log(colorNames(10)); // It'll log the first 10 colors
- * console.log(colorNames()); // It'll log all the colors
- * ```
- */
-declare function colorNames(amount?: number): string;
-
-export { colors, colorNames };
+  /**
+   * - The color object for Discord.js
+   * - It has 30 colors
+   * - It only works with Discord.js
+   */
+  declare const colors: colorsType;
+  
+  /**
+   * - 30 Colors
+   * @param {number} amount
+   * @returns all the colors name
+   * @example ```js
+   * console.log(colorNames(10)); // It'll log the first 10 colors
+   * console.log(colorNames()); // It'll log all the colors
+   * ```
+   */
+  declare function colorNames(amount?: number): string;
+  
+  export { colors, colorNames };
