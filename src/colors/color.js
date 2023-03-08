864f6584ef68e92107ccd7f865eec249b446b012
Mohtasim Alam Sohom <94981761+Sohom829@users.noreply.github.com>
2023-03-08 12:36:09 +0600

    Add colors

---
+++ src/colors/color.js
@@ -0,0 +1,11 @@
+const colors = {
+  red: 0xff0000,
+  orange: 0xffa500,
+  yellow: 0xffff00,
+  green: 0x008000,
+  blue: 0x0000ff,
+  indigo: 0x4b0082,
+  violet: 0xee82ee,
+};
+
+module.exports = colors;
