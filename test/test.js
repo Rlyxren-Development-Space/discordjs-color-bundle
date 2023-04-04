f601d4d4f1c98e88a54d37d21d86a2b289e50753
Mohtasim Alam Sohom <94981761+Sohom829@users.noreply.github.com>
2023-04-05 00:18:43 +0600

    Test

--- test/test.js
+++ test/test.js
@@ -1,7 +1,14 @@
-const { colors, colorNames, CustomHex } = require("../src");
+const {
+  colors,
+  colorNames,
+  CustomHex,
+  InternetColors,
+  ProductColors,
+} = require("../src");
 
 console.log(colors.SeaGreen);
-console.log(colorNames(colors));
+console.log(colorNames(colors, InternetColors, ProductColors));
 console.log(colors.RandomAdvanced);
 console.log(colors.WildWatermelon);
 console.log(CustomHex("#F"));
+console.log(InternetColors.Musics.MusicRecognition.MusixmatchLogo);
