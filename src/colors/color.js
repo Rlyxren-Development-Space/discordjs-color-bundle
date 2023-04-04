1dcea9cb06bc89975ce8aaf58f5bff6543025019
Mohtasim Alam Sohom <94981761+Sohom829@users.noreply.github.com>
2023-04-04 19:38:20 +0600

    Add 98 more colors

--- src/colors/color.js
+++ src/colors/color.js
@@ -82,6 +82,94 @@ const colors = {
   PastelPeach: 0xffb347,
   CandyPink: 0xff1cbd,
   PowderBlue: 0xb0e0e6,
+  Azzurro: 0x007fff,
+  AmaranthPink: 0xf19cbb,
+  Aubergine: 0x3b0910,
+  Byzantium: 0x702963,
+  CaputMortuum: 0x592720,
+  CeruleanFrost: 0x6d9bc3,
+  Coquelicot: 0xff3800,
+  DarkByzantium: 0x5d3954,
+  EtonBlue: 0x96c8a2,
+  Feldgrau: 0x4d5d53,
+  Gainsboro: 0xdcdcdc,
+  HeliotropeGray: 0xaa98a9,
+  InternationalKleinBlue: 0x002fa7,
+  Jasper: 0xd73b3e,
+  KhakiRose: 0xc8a2c8,
+  LemonCurry: 0xcca300,
+  Malachite: 0x0bda51,
+  MikadoYellow: 0xffc40c,
+  NadeshikoPink: 0xf6adc6,
+  Olivine: 0x9ab973,
+  PansyPurple: 0x78184a,
+  PapayaWhip: 0xffefd5,
+  Periwinkle: 0xccccff,
+  PersianPlum: 0x701c1c,
+  PhthaloGreen: 0x123524,
+  PineGreen: 0x01796f,
+  QuinacridoneMagenta: 0x8e3a59,
+  RoseMadder: 0xe32636,
+  SapphireBlue: 0x0067a5,
+  SeafoamGreen: 0x9fe2bf,
+  Smalt: 0x003399,
+  Tan: 0xd2b48c,
+  TaupeGray: 0x8b8589,
+  Tawny: 0xcd5700,
+  Tumbleweed: 0xdeaa88,
+  TyrianPurple: 0x66023c,
+  Ube: 0x8878c3,
+  Verdigris: 0x43bfc7,
+  ViridianGreen: 0x009698,
+  Wheat: 0xf5deb3,
+  Wisteria: 0xc9a0dc,
+  Xanadu: 0x738678,
+  YaleBlue: 0x0f4d92,
+  Zaffre: 0x0014a8,
+  Zomp: 0x39a78e,
+  Sarcoline: 0xffa07a,
+  Coquelicot: 0xff3800,
+  Sinopia: 0xcb410b,
+  Smaragdine: 0x50c878,
+  Glaucous: 0x6082b6,
+  AmaranthDeepPurple: 0x9f2b68,
+  BlueSapphire: 0x126180,
+  Byzantine: 0xbd33a4,
+  CeladonGreen: 0x2f847c,
+  Cinnabar: 0xe34234,
+  CobaltBlue: 0x0047ab,
+  CoralRed: 0xff4040,
+  CurrantRed: 0x9b111e,
+  DarkSeaGreen: 0x8fbc8f,
+  DeepCarminePink: 0xef3038,
+  DeepJungleGreen: 0x004b49,
+  DeepViolet: 0x330066,
+  ElectricIndigo: 0x6f00ff,
+  Fandango: 0xb53389,
+  Heliotrope: 0xdf73ff,
+  IndigoDye: 0x091f92,
+  Iridescent: 0x6e819f,
+  LavenderGray: 0xc4c3d0,
+  Lava: 0xcf1020,
+  LemonGlacier: 0xfdff00,
+  LightSlateGray: 0x778899,
+  MaximumBlueGreen: 0x30bfbf,
+  MidnightGreen: 0x004953,
+  OldRose: 0xc08081,
+  OtterBrown: 0x654321,
+  PaleTaupe: 0xbc987e,
+  PeachPuff: 0xffdab9,
+  PersianBlue: 0x1c39bb,
+  Phlox: 0xdf00ff,
+  Pineapple: 0x563c5c,
+  PrussianBlue: 0x003153,
+  Raspberry: 0xe30b5c,
+  RichBlack: 0x010203,
+  RoseGold: 0xb76e79,
+  Russet: 0x80461b,
+  SmokyBlack: 0x100c08,
+  SpanishGray: 0x989898,
+  TiffanyBlue: 0x0abab5,
   RandomAdvanced: getRandomHexColor(),
 };
 
@@ -159,20 +247,43 @@ function AmountError(message) {
 
 AmountError.prototype = Object.create(Error.prototype);
 
-function colorNames(amount) {
-  if (typeof amount === "undefined") {
-    amount = 60;
+function colorNames(colors) {
+  if (typeof colors === "undefined") {
+    throw new Error("No colors object provided.");
   }
-  if (amount < 0 || amount > 60) {
-    throw new AmountError(`
-  Whoa there, hold on a sec! We've got a problem here. The amount you entered is not within the acceptable range. It should be between 0 and 60, but you entered ${amount}. Let's try that again with a value within the specified range, shall we?`);
-  }
-  if (typeof amount !== "number") {
+  if (typeof colors !== "object") {
     throw new TypeError(`
-  Expected type to be "number" but got "${typeof amount}" instead.`);
+  Expected colors to be an "object" but got "${typeof colors}" instead.`);
   }
-  var colors = colorArray.slice(0, amount);
-  return colors.join(", ");
+  var colorNames = Object.keys(colors);
+  var formattedColors = "";
+  var responseTime = new Date();
+  for (var i = 0; i < colorNames.length; i++) {
+    var colorName = colorNames[i];
+    var colorValue = colors[colorName];
+    var ansiColor = hexToAnsi(colorValue);
+    formattedColors += `${
+      i + 1
+    }. \x1b[1m${colorName}\x1b[0m ${ansiColor}${ansiColor}\n`;
+  }
+  var totalColors = colorNames.length;
+  var elapsedTime = new Date() - responseTime;
+  formattedColors += `\u001b[1mTotal Colors: \u001b[36m${totalColors}\u001b[0m\n\u001b[1mResponse Time: \u001b[33m${elapsedTime}ms\u001b[0m`;
+
+  return formattedColors;
+}
+
+function hexToAnsi(hexValue) {
+  var hexString = hexValue.toString(16).padStart(6, "0");
+  var r = parseInt(hexString.substring(0, 2), 16);
+  var g = parseInt(hexString.substring(2, 4), 16);
+  var b = parseInt(hexString.substring(4, 6), 16);
+  var ansiColorCode =
+    16 +
+    36 * Math.round((r / 255) * 5) +
+    6 * Math.round((g / 255) * 5) +
+    Math.round((b / 255) * 5);
+  return `\u001b[38;5;${ansiColorCode}m`;
 }
 
 module.exports = {
