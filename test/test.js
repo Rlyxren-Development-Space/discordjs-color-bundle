be2f7d1d2d2cff63de8a42c19298e210161318f1
Mohtasim Alam Sohom <94981761+Sohom829@users.noreply.github.com>
2023-04-04 19:39:10 +0600

    Test if it works

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 const { colors, colorNames, CustomHex } = require("../src");
 
 console.log(colors.SeaGreen);
-console.log(colorNames(45));
+console.log(colorNames(colors));
 console.log(colors.RandomAdvanced);
 console.log(colors.WildWatermelon);
 console.log(CustomHex("#FFFFF"));
