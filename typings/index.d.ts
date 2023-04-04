2f9143d55ab6091015110071bfbb812fb7f892b6
Mohtasim Alam Sohom <94981761+Sohom829@users.noreply.github.com>
2023-04-04 19:38:59 +0600

    Update declaration

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -3,69 +3,15 @@ declare function getRandomHexColor(): number;
 declare function CustomHex(hexCode: string): number;
 
 declare interface DiscordColors {
-  Red: number;
-  Orange: number;
-  Yellow: number;
-  Green: number;
-  Blue: number;
-  Indigo: number;
-  Violet: number;
-  Purple: number;
-  Lime: number;
-  LemonJuice: number;
-  CadetBlue: number;
-  HotPink: number;
-  CornFlowerBlue: number;
-  Brick: number;
-  OliveDrab: number;
-  Olive: number;
-  Coral: number;
-  LightCoral: number;
-  Cyan: number;
-  LightCyan: number;
-  Aqua: number;
-  Aquamarine: number;
-  Salmon: number;
-  SeaGreen: number;
-  SeaBlue: number;
-  LightSeaGreen: number;
-  SkyBlue: number;
-  LightSkyBlue: number;
-  SpringGreen: number;
-  Tomato: number;
-  Chartreuse: number;
-  DarkChartreuse: number;
-  SandyBrown: number;
-  Plum: number;
-  MysticMauve: number;
-  ElectricKiwi: number;
-  VelvetMaroon: number;
-  TurquoiseSurf: number;
-  MidnightSun: number;
-  CherryBlossomPink: number;
-  LavenderHaze: number;
-  CaramelLatte: number;
-  Goldenrod: number;
-  IndigoNight: number;
-  SeafoamSplash: number;
-  LimeFizz: number;
-  SapphireDepths: number;
-  MintMist: number;
-  FrostyFern: number;
-  MidnightMarina: number;
-  WildStrawberry: number;
-  DeepAqua: number;
-  BerrySorbet: number;
-  DreamyLavender: number;
-  WildWatermelon: number;
-  FrostyMint: number;
-  PeachyPink: number;
-  PastelPeach: number;
-  CandyPink: number;
-  PowderBlue: number;
-  RandomAdvanced: number;
+  [key: number]: number;
 }
+
+interface ColorsObject {
+  obj: object;
+}
+
+declare function colorNames(obj: ColorsObject): string;
 
 declare const colors: DiscordColors;
 
-export { getRandomHexColor, CustomHex, colors };
+export { getRandomHexColor, CustomHex, colors, colorNames };
