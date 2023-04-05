6e3fcc653d207ab8cb7916f49e67ba98be10ccc8
Mohtasim Alam Sohom <94981761+Sohom829@users.noreply.github.com>
2023-04-05 22:58:33 +0600

    Update typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,10 +1,64 @@
-/**
+type RedInput = number;
+type GreenInput = number;
+type BlueInput = number;
+type AlphaInput = number;
+type HexValue = number;
+type HexCode = number;
+type RandomHex = number;
 
-Returns a random hexadecimal color as a number.
-*/
-declare function getRandomHexColor(): number;
-/**
+class ColorConvertError extends Error {
+  constructor(prefix: string, ...message: string[]) {
+    super(`[${prefix}]: ${message.join(" ")}`);
+    this.name = "ColorConvertError";
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, ColorConvertError);
+    }
+  }
+}
 
+/**
+ * Returns a random hexadecimal color value as a number.
+ * @returns {RandomHex} RandomHex
+ */
+declare function getRandomHexColor(): RandomHex;
+
+/**
+ * Converts a hexadecimal color code to a number.
+ * @param {string} hexCode The hexadecimal color code.
+ * @returns {number} The converted number.
+ * @throws {ColorConvertError} If the input is not a string or is an invalid format.
+ */
+declare function CustomHex(hexCode: string): number;
+
+/**
+ * Converts RGB color values to a hexadecimal color code.
+ * @param {RedInput} red The red color value.
+ * @param {GreenInput} green The green color value.
+ * @param {BlueInput} blue The blue color value.
+ * @returns {HexValue} The hexadecimal color code.
+ */
+declare function CustomRGB(
+  red: RedInput,
+  green: GreenInput,
+  blue: BlueInput
+): HexValue;
+
+/**
+ * Converts RGBA color values to a hexadecimal color code.
+ * @param {RedInput} red The red color value.
+ * @param {GreenInput} green The green color value.
+ * @param {BlueInput} blue The blue color value.
+ * @param {AlphaInput} alpha The alpha (transparency) value.
+ * @returns {HexCode} The hexadecimal color code.
+ */
+declare function CustomRGBA(
+  red: RedInput,
+  green: GreenInput,
+  blue: BlueInput,
+  alpha: AlphaInput
+): HexCode;
+
+/**
 Returns the number representation of the given hexadecimal color code.
 @param hexCode The hexadecimal color code (e.g. '#ff0000').
 @returns The number representation of the color.
@@ -53,11 +107,13 @@ declare const internetColors: InternetColors;
 declare const productColors: ProductColors;
 export {
   getRandomHexColor,
-  CustomHex,
   colors,
   internetColors,
   productColors,
   Colors,
   InternetColors,
   ProductColors,
+  CustomRGB,
+  CustomHex,
+  CustomRGBA,
 };
