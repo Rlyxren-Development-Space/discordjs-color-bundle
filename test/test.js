e861d373d2de4ec33a0cea6a20f4318df35c40bd
Mohtasim Alam Sohom <94981761+Sohom829@users.noreply.github.com>
2023-04-05 22:59:16 +0600

    Test RGB and RGBA parser

--- test/test.js
+++ test/test.js
@@ -4,6 +4,8 @@ const {
   CustomHex,
   InternetColors,
   ProductColors,
+  CustomRGB,
+  CustomRGBA,
 } = require("../src");
 
 console.log(colors.SeaGreen);
@@ -11,4 +13,5 @@ console.log(colorNames(colors, InternetColors, ProductColors));
 console.log(colors.RandomAdvanced);
 console.log(colors.WildWatermelon);
 console.log(CustomHex("#F"));
+console.log(CustomRGBA(200, 41, 11, 1));
 console.log(InternetColors.Musics.MusicRecognition.MusixmatchLogo);
