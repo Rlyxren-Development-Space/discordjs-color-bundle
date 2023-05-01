315493ed130ef97b105f8a9a42d22a75f2b64894
Mohtasim Alam Sohom <94981761+Sohom829@users.noreply.github.com>
2023-05-01 11:31:01 +0600

    Update TypeScript and JavaScript intelligence

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -6,16 +6,6 @@ type HexValue = number;
 type HexCode = number;
 type RandomHex = number;
 
-class ColorConvertError extends Error {
-  constructor(prefix: string, ...message: string[]) {
-    super(`[${prefix}]: ${message.join(" ")}`);
-    this.name = "ColorConvertError";
-    if (Error.captureStackTrace) {
-      Error.captureStackTrace(this, ColorConvertError);
-    }
-  }
-}
-
 /**
  * Returns a random hexadecimal color value as a number.
  * @returns {RandomHex} RandomHex
@@ -69,21 +59,225 @@ declare function CustomHex(hexCode: string): number;
 An object containing color names as keys and their corresponding number representation as values.
 */
 interface Colors {
-  [key: number]: number;
+  Red: number;
+  Orange: number;
+  Yellow: number;
+  Green: number;
+  Blue: number;
+  Indigo: number;
+  Violet: number;
+  Purple: number;
+  Lime: number;
+  LemonJuice: number;
+  CadetBlue: number;
+  HotPink: number;
+  CornFlowerBlue: number;
+  Brick: number;
+  OliveDrab: number;
+  Olive: number;
+  Coral: number;
+  LightCoral: number;
+  Cyan: number;
+  LightCyan: number;
+  Aqua: number;
+  Aquamarine: number;
+  Salmon: number;
+  SeaGreen: number;
+  SeaBlue: number;
+  LightSeaGreen: number;
+  SkyBlue: number;
+  LightSkyBlue: number;
+  SpringGreen: number;
+  Tomato: number;
+  Chartreuse: number;
+  DarkChartreuse: number;
+  SandyBrown: number;
+  Plum: number;
+  MysticMauve: number;
+  ElectricKiwi: number;
+  VelvetMaroon: number;
+  TurquoiseSurf: number;
+  MidnightSun: number;
+  CherryBlossomPink: number;
+  LavenderHaze: number;
+  CaramelLatte: number;
+  Goldenrod: number;
+  IndigoNight: number;
+  SeafoamSplash: number;
+  LimeFizz: number;
+  SapphireDepths: number;
+  MintMist: number;
+  FrostyFern: number;
+  MidnightMarina: number;
+  WildStrawberry: number;
+  DeepAqua: number;
+  BerrySorbet: number;
+  DreamyLavender: number;
+  WildWatermelon: number;
+  FrostyMint: number;
+  PeachyPink: number;
+  PastelPeach: number;
+  CandyPink: number;
+  PowderBlue: number;
+  Azzurro: number;
+  AmaranthPink: number;
+  Aubergine: number;
+  Byzantium: number;
+  CaputMortuum: number;
+  CeruleanFrost: number;
+  Coquelicot: number;
+  DarkByzantium: number;
+  EtonBlue: number;
+  Feldgrau: number;
+  Gainsboro: number;
+  HeliotropeGray: number;
+  InternationalKleinBlue: number;
+  Jasper: number;
+  KhakiRose: number;
+  LemonCurry: number;
+  Malachite: number;
+  MikadoYellow: number;
+  NadeshikoPink: number;
+  Olivine: number;
+  PansyPurple: number;
+  PapayaWhip: number;
+  Periwinkle: number;
+  PersianPlum: number;
+  PhthaloGreen: number;
+  PineGreen: number;
+  QuinacridoneMagenta: number;
+  RoseMadder: number;
+  SapphireBlue: number;
+  SeafoamGreen: number;
+  Smalt: number;
+  Tan: number;
+  TaupeGray: number;
+  Tawny: number;
+  Tumbleweed: number;
+  TyrianPurple: number;
+  Ube: number;
+  Verdigris: number;
+  ViridianGreen: number;
+  Wheat: number;
+  Wisteria: number;
+  Xanadu: number;
+  YaleBlue: number;
+  Zaffre: number;
+  Zomp: number;
+  Sarcoline: number;
+  Coquelicot: number;
+  Sinopia: number;
+  Smaragdine: number;
+  Glaucous: number;
+  AmaranthDeepPurple: number;
+  BlueSapphire: number;
+  Byzantine: number;
+  CeladonGreen: number;
+  Cinnabar: number;
+  CobaltBlue: number;
+  CoralRed: number;
+  CurrantRed: number;
+  DarkSeaGreen: number;
+  DeepCarminePink: number;
+  DeepJungleGreen: number;
+  DeepViolet: number;
+  ElectricIndigo: number;
+  Fandango: number;
+  Heliotrope: number;
+  IndigoDye: number;
+  Iridescent: number;
+  LavenderGray: number;
+  Lava: number;
+  LemonGlacier: number;
+  LightSlateGray: number;
+  MaximumBlueGreen: number;
+  MidnightGreen: number;
+  OldRose: number;
+  OtterBrown: number;
+  PaleTaupe: number;
+  PeachPuff: number;
+  PersianBlue: number;
+  Phlox: number;
+  Pineapple: number;
+  PrussianBlue: number;
+  Raspberry: number;
+  RichBlack: number;
+  RoseGold: number;
+  Russet: number;
+  SmokyBlack: number;
+  SpanishGray: number;
+  TiffanyBlue: number;
+  RandomAdvanced: Function;
 }
 /**
 
 An object containing internet app color names as keys and their corresponding number representation as values.
 */
 interface InternetColors {
-  [key: number]: number;
+  SocialMedia: {
+    FacebookLogo: number;
+    TwitterLogo: number;
+    InstagramLogo: number;
+    LinkedInLogo: number;
+    SnapchatLogo: number;
+  };
+  Communication: {
+    DiscordLogo: number;
+    ZoomLogo: number;
+    SkypeLogo: number;
+    SlackLogo: number;
+  };
+  ECommerce: {
+    AmazonLogo: number;
+    eBayLogo: number;
+    ShopifyLogo: number;
+  };
+  Musics: {
+    MusicStreaming: {
+      SpotifyLogo: number;
+      AppleMusicLogo: number;
+      TidalLogo: number;
+    };
+    MusicRecognition: {
+      ShazamLogo: number;
+      MusixmatchLogo: number;
+    };
+    MusicalInstruments: {
+      FenderLogo: number;
+      GibsonLogo: number;
+      YamahaLogo: number;
+    };
+  };
 }
 /**
 
 - An object containing product color names as keys and their corresponding number representation as values.
 */
 interface ProductColors {
-  [key: number]: number;
+  Soap: {
+    DettolOriginalSoap: number;
+    LifebuoyTotal10Soap: number;
+    DoveWhiteBeautyBarSoap: number;
+    IvoryOriginalBathSoap: number;
+  };
+  Electronics: {
+    RazerBlade15GamingLaptop: number;
+    AppleiPhone13ProMax: number;
+    SamsungGalaxyS21Ultra: number;
+    SonyPlayStation5: number;
+  };
+  FoodAndBeverages: {
+    CocaColaClassic: number;
+    Pepsi: number;
+    KitKat: number;
+    Oreo: number;
+  };
+  Clothing: {
+    NikeAirMax97: number;
+    AdidasOriginalsSuperstar: number;
+    Levis501OriginalJeans: number;
+    HAndMBasicTshirt: number;
+  };
 }
 /**
 
