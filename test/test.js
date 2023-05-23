0fa9bf017a9612093d3709b797f99a5fabef1aa0
Mohtasim Alam Sohom <94981761+Sohom829@users.noreply.github.com>
2023-05-23 11:21:46 +0600

    Update test.js

--- test/test.js
+++ test/test.js
@@ -2,19 +2,19 @@ const {
   colors,
   colorNames,
   CustomHex,
-  InternetColors,
-  ProductColors,
+  internetColors,
+  productColors,
   CustomRGB,
   CustomRGBA,
-} = require("../src");
+} = require("../colors-bundle");
 const obj = {
   value1: "Shax",
 };
 
-console.log(colors.SeafoamSplash);
-console.log(colorNames(colors, InternetColors, ProductColors));
+console.log(colors.AmberRose);
+console.log(colorNames(colors));
 console.log(colors.RandomAdvanced);
 console.log(colors.WildWatermelon);
 console.log(CustomHex("#F"));
 console.log(CustomRGBA(200, 41, 11, 1));
-console.log(InternetColors.Musics.MusicRecognition.MusixmatchLogo);
+console.log(internetColors.Musics.MusicRecognition.MusixmatchLogo);
